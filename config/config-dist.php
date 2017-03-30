f81ccf5416a4fee932b1aa6453d2fc583e454164
Sion <sionix1@ya.ru>
2017-03-30 23:00:33 +0300

    removed config, added config-dist.php

---
+++ config/config-dist.php
@@ -0,0 +1,17 @@
+<?php
+$config = [
+	"settings" => [
+		'displayErrorDetails' => true,
+		'db' => [
+			'driver' => 'mysql',
+			'host' => 'localhost',
+			'database' => 'fileshare',
+			'username' => 'root',
+			'password' => '',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		],
+		"dir" => "/var/www/fileshare.home/files"
+	]
+];
